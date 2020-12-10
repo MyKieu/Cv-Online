85da771a318c7d62a0101b97669e67abc90227a9
Mykieu <huynhthimykieu9999@gmail.com>
2020-12-10 10:52:19 +0700

    deleteform

--- src/utils/fake_data.js
+++ src/utils/fake_data.js
@@ -1,43 +1,43 @@
 
 const data = {
   header: {
-    name: "Huỳnh Thị Mỹ Kiều",
+    name: "Huynh Thi My Kieu",
     email: "huynhthimykieu9999@gmail.com",
-    gender: "Nữ",
+    gender: "Female",
     phone: "0345411942",
-    address: "29 Phạm Như Xương",
+    address: "29 Pham Nhu Xuong",
     city:'Da Nang'
   },
   education: {
-    institition: "Đại Học Sư Phạm Đà Nẵng",
-    city: "TP Đà Nẵng",
-    major: "Công nghệ thông tin",
+    institition: "Dai hoc Su Pham Da Nang",
+    city: "Da Nang",
+    major: "Information Technology",
     graduation: "6/2021",
     add: "GPA :3",
   },
   experience: {
     company: "CompleteCode",
-    city: "Đà Nẵng",
+    city: "Da Nang",
     position: "Fresher Frontend",
     start: "1/10/2019",
     end: "30/1/2020",
     description: [
-      "Có các kiến thức nền về lập trình",
-      "Kĩ năng làm việc nhóm",
-      "Hòa đồng vui vẻ",
+      "knowledge about programming",
+      "Skill practice with teamwork",
+      "Fun and frendly with everyone",
     ],
-    company2: "Trung tâm phát triển phần mềm",
-    city2: "Đà Nẵng",
+    company2: "Sotfware development centre",
+    city2: "Da Nang",
     position2: "Internship React",
     start2: "15/11/2020",
     end2: "6/3/2020",
-    description2: ["Cùng team thực hiện task ", "Trao đổi bằng tiếng anh"],
+    description2: ["Share experience code with co-worker ", "Communication anhd practice skill English  ","Take part in sport"],
   },
-  skill: [
-    "English speaker",
+skill:
+  [
     "Adaptability",
     "Interpersonal Communication",
     "Friend of all",
-  ],
+  ]
 };
 export default data;
